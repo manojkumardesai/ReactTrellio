755701a4cddffbd41da9eea3d850a30382829f4f
Manoj Kumar Desai <manoj@itemmaster.com>
2018-06-17 22:08:18 +0530

    Components added, also added initial state of redux.

---
+++ src/store/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    list : [{
+        id: 0,
+        name: 'First List',
+        tasks: []
+    }]
+}
+
+const reducer = (state = initialState, action) => {
+    return state;
+}
+
+export default reducer;
